0c50fad0447d211bf703b74592731f84b351283f
SasukeKun007 <gbudhalakoti@gmail.com>
2024-01-06 00:17:39 +0530

    kk

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("hey world!");
+const { Client, Collection } = require("discord.js");
+
+const client = new Client({
+    intents: 3276799,
+    partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"],
+    allowedMentions: {
+        repliedUser: true,
+        parse: ["everyone", "roles", "users"]
+    }
+});
+
+module.exports = client;
+
+client.emoji = require("./emojis.json");
+client.config = require("./config.js");
+client.events = new Collection();
+client.commands = new Collection();
+require(`${process.cwd()}/handler/main.js`);
+
+const token = client.config.token
+client.login(token);
+if(!token || typeof token !== "string") {
+    throw new Error("Error [TokenInvalid]: Provide a valid token in config.js")
+}
