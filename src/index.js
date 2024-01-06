5e6bacd2473b2caa304f822351af986160d773ff
SasukeKun007 <gbudhalakoti@gmail.com>
2024-01-06 16:26:32 +0530

    real

--- src/index.js
+++ src/index.js
@@ -15,10 +15,18 @@ client.emoji = require("./emojis.json");
 client.config = require("./config.js");
 client.events = new Collection();
 client.commands = new Collection();
-require(`${process.cwd()}/handler/main.js`);
+require(`${process.cwd()}/src/handler/main.js`);
 
 const token = client.config.token
 client.login(token);
 if(!token || typeof token !== "string") {
-    throw new Error("Error [TokenInvalid]: Provide a valid token in config.js")
+    throw new Error("[TokenInvalid]: Provide a valid token in config.js")
 }
+
+process.on('unhandledRejection', async (er) => {
+    console.log(er);
+});
+
+process.on('uncaughtException', async (err) => {
+    console.log(err);
+})
