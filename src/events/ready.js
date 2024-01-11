5421674e2d34113cdde388a14e637cc01a0c14ff
Punit <punitkumar8728@gmail.com>
2024-01-11 14:31:12 +0000

    changes

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,6 +5,13 @@ class BaseReady {
         this.event = "ReadyEvent";
     }
     run() {
+        this.client.user.setPresence({
+            activities: [
+                name: "Base Client",
+                type: 2
+            ],
+            status: "customStatus"
+        });
         this.client.logger.ready(`Client is Ready with Username: ${this.client.user.username} and tag: ${this.client.user.tag}!`);
     }
 }
