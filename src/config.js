e93df83922386d7a109f4463f2ce5cd7bdfe6c44
SasukeKun007 <gbudhalakoti@gmail.com>
2024-01-11 17:51:28 +0530

    Removed some files!

--- src/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    "token" : "MTE1NDY4NTk5NTYxOTQ1NTAwNg.G6dSSo.fhjp30w_2ZupBh2HoSBmxTfgBWiav-VlQaGa3Y",
-};
-
-module.exports = config;
--- src/events/ready.js
+++
@@ -1,3 +0,0 @@
-module.exports = async (client) => {
-    console.log(`${client.user.username} is online and ready to serve ${client.users.cache.size} users in ${client.guilds.cache.size} servers.`)
-}
